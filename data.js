5e4c5500622f1601ea56cb15d558624daaa2b93c
lawler-coder <lawler@co2efficient.com>
2025-10-30 08:46:49 -0400

    Refine building coordinates with accurate lat/lng
    Updated latitude and longitude coordinates for several buildings.

--- data.js
+++ data.js
@@ -4,11 +4,11 @@ const BUILDING_COORDS = {
   'Cannon House Office Building': {
     address: 'Cannon House Office Building, Washington, DC 20515',
     // TODO: paste exact lat/lng from Google Maps (see Step 2)
-    lat: 38.886811, lng: null
+    lat: 38.88668839987316, lng: -77.00694138122337
   },
   'Longworth House Office Building': {
     address: 'Longworth House Office Building, Washington, DC 20515',
-    lat: null, lng: null
+    lat: 38.88651283713331, lng: -77.0083668319861
   },
   'Rayburn House Office Building': {
     address: 'Rayburn House Office Building, Washington, DC 20515',
@@ -16,16 +16,17 @@ const BUILDING_COORDS = {
   },
   'Hart Senate Office Building': {
     address: '120 Constitution Ave NE, Washington, DC 20510',
-    lat: null, lng: null
+    lat: 38.89287825629864, lng: -77.00483097397986
   },
   'Dirksen Senate Office Building': {
     address: '50 Constitution Ave NE, Washington, DC 20510',
-    lat: null, lng: null
+    lat: 38.893070317087286, lng: -77.00551761952482
   },
   'Russell Senate Office Building': {
     address: '2 Constitution Ave NE, Washington, DC 20510',
-    lat: null, lng: null
+    lat: 38.892636091955296, lng: -77.00691236828801
   }
 };
+
 
 
